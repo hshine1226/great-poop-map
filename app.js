de22caf715dd2594f1d54bcd6fde917924dce864
hshine1226 <hshine1226@gmail.com>
2020-06-19 15:28:23 +0900

    Use router
    add home, toilets, users routers

---
+++ app.js
@@ -0,0 +1,26 @@
+import express from "express";
+// Middlewares
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import routes from "./routes";
+import globalRouter from "./routers/globalRouter";
+import toiletRouter from "./routers/toiletRouter";
+import usersRouter from "./routers/usersRouter";
+
+const app = express();
+
+// Middleware
+app.use(morgan("dev")); // Logging에 도움을 주는 미들웨어
+app.use(helmet()); // Express App에 도움을 주는 미들웨어
+app.use(bodyParser.json()); // 서버가 json을 이해하게 해주는 미들웨어
+app.use(bodyParser.urlencoded({ extended: true })); // 서버가 urlencoded를 이해하게 해주는 미들웨어
+app.use(cookieParser()); // Session을 다루기 위한 미들웨어
+
+// Routers
+app.use(routes.home, globalRouter);
+app.use(routes.toilets, toiletRouter);
+app.use(routes.users, usersRouter);
+
+export default app;
