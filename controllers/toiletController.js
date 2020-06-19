047e711e288a4a5f85f921b580408c34322c5e0c
hshine1226 <hshine1226@gmail.com>
2020-06-19 15:26:14 +0900

    Add Controllers
    toiletController, userController

---
+++ controllers/toiletController.js
@@ -0,0 +1,6 @@
+export const getHome = (req, res) => res.send("Home");
+export const getToilets = (req, res) => res.send("Toilets");
+export const getAdd = (req, res) => res.send("Add");
+export const getToiletDetail = (req, res) => res.send("Toilet detail");
+export const getEditToilet = (req, res) => res.send("Toilet edit");
+export const getDeleteToilet = (req, res) => res.send("Toilet delete");
---
+++ controllers/userControllers.js
@@ -0,0 +1,8 @@
+export const getJoin = (req, res) => res.send("Join");
+export const getLogin = (req, res) => res.send("Login");
+export const getLogout = (req, res) => res.send("Logout");
+export const getUsers = (req, res) => res.send("Users");
+export const getEditProfile = (req, res) => res.send("Edit profile");
+export const getChangePassword = (req, res) => res.send("Change password");
+export const getUserDetail = (req, res) => res.send("User detail");
+export const getMe = (req, res) => res.send("Me");
