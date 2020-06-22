720897451a4d88e4df9bf5f2cd8b9eefb5259b70
hshine1226 <hshine1226@gmail.com>
2020-06-23 02:32:29 +0900

    Add marker cluster on kakao maps

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,8 +1,9 @@
+// console.log(res);
 // 지도 생성
 const container = document.getElementById("map");
 const options = {
   center: new kakao.maps.LatLng(36.3154865690411, 127.446044443222),
-  level: 6,
+  level: 4,
 };
 const map = new kakao.maps.Map(container, options);
 
@@ -13,28 +14,36 @@ let callback = function (result, status) {
   if (status === kakao.maps.services.Status.OK) {
     const longitude = result[0].x;
     const latitude = result[0].y;
-    console.log(result);
-    console.log(latitude, longitude);
+    console.log(longitude, latitude);
+    // console.log(result[0]);
   }
 };
-
-geocoder.addressSearch("대전시 동구 대전로 542번길 78", callback);
+geocoder.addressSearch(
+  "대구광역시 달성군 다사읍 매곡리 1544번지 2호",
+  callback
+);
 
 // 클러스터에 마커 추가
-var clusterer = new kakao.maps.MarkerClusterer({
+const clusterer = new kakao.maps.MarkerClusterer({
   map: map,
   averageCenter: true,
   minLevel: 6,
   disableClickZoom: true,
 });
 
-var myLocation = new kakao.maps.Marker({
+var marker = new kakao.maps.Marker({
   position: new kakao.maps.LatLng(36.3154865690411, 127.446044443222),
 });
 
-clusterer.addMarker(myLocation);
+clusterer.addMarker(marker);
 
+// 마커 클러스터러에 클릭이벤트를 등록합니다
+// 마커 클러스터러를 생성할 때 disableClickZoom을 true로 설정하지 않은 경우
+// 이벤트 헨들러로 cluster 객체가 넘어오지 않을 수도 있습니다
 kakao.maps.event.addListener(clusterer, "clusterclick", function (cluster) {
-  console.log(cluster.getCenter());
-  console.log("inin");
+  // 현재 지도 레벨에서 1레벨 확대한 레벨
+  const level = map.getLevel() - 1;
+
+  // 지도를 클릭된 클러스터의 마커의 위치를 기준으로 확대합니다
+  map.setLevel(level, { anchor: cluster.getCenter() });
 });
