543b71b595582b22a6a9bc5e5a557f963569e9fe
hshine1226 <hshine1226@gmail.com>
2020-06-23 22:26:26 +0900

    Add connection mongo db & mongoose

---
+++ db.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/great-poop-map", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ connected to DB");
+const handleError = (error) =>
+  console.log(`❌ Error on DB connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
