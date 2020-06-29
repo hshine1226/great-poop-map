60bbde80254beca06890f2e7d1e2c4847fb0ca4d
hshine1226 <hshine1226@gmail.com>
2020-06-29 16:15:06 +0900

    Add info window on the marker

--- assets/js/map.js
+++ assets/js/map.js
@@ -5,9 +5,20 @@ import axios from "axios";
 const container = document.getElementById("map");
 const options = {
   center: new kakao.maps.LatLng(36.3154865690411, 127.446044443222),
-  level: 1,
+  level: 4,
 };
 const map = new kakao.maps.Map(container, options);
+
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+const mapTypeControl = new kakao.maps.MapTypeControl();
+
+// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+const zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
 
 // 드래그 가능한 마커 생성하기
 const setMarker = (latitude, longitude) => {
@@ -71,18 +82,48 @@ const getToilets = async (latt, long) => {
       const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize);
 
       // 마커를 생성합니다
+      const latLng = new kakao.maps.LatLng(latt, long);
       const marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
         position: new kakao.maps.LatLng(latt, long), // 마커를 표시할 위치
         title: toilets[idx].name, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
-
         image: markerImage, // 마커 이미지
       });
 
+      // 마커에 표시할 인포윈도우를 생성합니다
+      const infowindow = new kakao.maps.InfoWindow({
+        content: toilets[idx].name, // 인포윈도우에 표시할 내용
+      });
+
+      kakao.maps.event.addListener(
+        marker,
+        "mouseover",
+        makeOverListener(map, marker, infowindow)
+      );
+      kakao.maps.event.addListener(
+        marker,
+        "mouseout",
+        makeOutListener(infowindow)
+      );
       marker.setMap(map);
     }
   }
 };
+
+// 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+const makeOverListener = (map, marker, infowindow) => {
+  return function () {
+    infowindow.open(map, marker);
+  };
+};
+
+// 인포윈도우를 닫는 클로저를 만드는 함수입니다
+const makeOutListener = (infowindow) => {
+  return function () {
+    infowindow.close();
+  };
+};
+
 const setNearMarker = () => {
   const setBtn = document.getElementById("jsGetToilets");
   const handleClick = () => {
