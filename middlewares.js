c1ae71d1bb5e52538364ee2b751df181ff7b7e1c
hshine1226 <hshine1226@gmail.com>
2020-07-05 09:29:58 +0900

    Use multer for uploading user avatars

--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,11 @@
 import routes from "./routes";
+import multer from "multer";
+
+const multerAvatar = multer({
+  dest: "uploads/avatars/",
+});
+
+export const uploadAvatar = multerAvatar.single("avatar");
 
 export const localsMiddleware = (req, res, next) => {
   res.locals.siteName = "대똥여지도";
