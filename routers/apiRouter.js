6f0d4381ca7b686ec9d8aeeda6e1854186d0a49a
hshine1226 <hshine1226@gmail.com>
2020-07-09 02:36:45 +0900

    Add checkEmail router

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -4,11 +4,13 @@ import {
   getBoxToilet,
   getNearToilets,
   getToiletDetail,
+  checkEmail,
 } from "../src/controllers/apiController";
 
 const apiRouter = express.Router();
 apiRouter.get(routes.nearToilets, getNearToilets);
 apiRouter.post(routes.boxToilet, getBoxToilet);
 apiRouter.get(routes.toiletDetail, getToiletDetail);
+apiRouter.get(routes.checkEmail, checkEmail);
 
 export default apiRouter;
