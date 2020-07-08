72bc9df6cc2d981ed9f7dcf98cf43fd845e98c6f
hshine1226 <hshine1226@gmail.com>
2020-07-09 02:35:50 +0900

    Add schema
    gender and default of avatarUrl

--- models/User.js
+++ models/User.js
@@ -4,8 +4,18 @@ import passportLocalMongoose from "passport-local-mongoose";
 const userSchema = new mongoose.Schema({
   name: String,
   nickname: String,
-  email: String,
-  avatarUrl: String,
+  email: { type: String, unique: true },
+  // Add 'enum' of an array of options to force selection between a given number of options.
+  // Anything other than "male" or "female" will be invalid.
+  gender: {
+    type: String,
+    enum: ["male", "female"],
+  },
+  avatarUrl: {
+    type: String,
+    default:
+      "blob:https://fontawesome.com/080baef0-6a6e-4131-aa66-cf316ee95a59",
+  },
   naverId: Number,
   googleId: Number,
   kakaoId: Number,
