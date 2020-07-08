f5daf3803dd6ba1256fb10d9bd96b4ff3d84620c
hshine1226 <hshine1226@gmail.com>
2020-07-09 02:37:00 +0900

    Add getComment controller

--- routers/toiletRouter.js
+++ routers/toiletRouter.js
@@ -6,12 +6,14 @@ import {
   getToiletDetail,
   getEditToilet,
   getDeleteToilet,
+  getComments,
 } from "../src/controllers/toiletController";
 
 const toiletRouter = express.Router();
 
 toiletRouter.get(routes.home, getToilets);
 toiletRouter.get(routes.add, getAdd);
+toiletRouter.get(routes.comments, getComments);
 toiletRouter.get(routes.toiletDetail, getToiletDetail);
 toiletRouter.get(routes.editToilet, getEditToilet);
 toiletRouter.get(routes.deleteToilet, getDeleteToilet);
