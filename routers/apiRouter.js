b0646992b860e73adc6a0c854b94f4ebc85fbba6
hshine1226 <hshine1226@gmail.com>
2020-07-08 13:28:24 +0900

    Simplify api router

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -1,17 +1,9 @@
 import express from "express";
 import routes from "../routes";
-import {
-  getMaps,
-  postAddLatLng,
-  getToilets,
-  getToiletDetail,
-} from "../src/controllers/apiController";
+import { getToilets, getBoxToilet } from "../src/controllers/apiController";
 
 const apiRouter = express.Router();
-
-apiRouter.get(routes.addMap, getMaps);
-apiRouter.get(routes.addLatLng, postAddLatLng);
 apiRouter.get(routes.getToilets, getToilets);
-apiRouter.get(routes.getToiletDetail, getToiletDetail);
+apiRouter.post(routes.getBoxToilet, getBoxToilet);
 
 export default apiRouter;
